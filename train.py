e98bedf19e23e9c3bffd9d097f6c6f0394f3f835
Gabir Yusuf <gabir.yusuf@gmail.com>
2019-04-27 21:08:34 +0200

    Fix import error

--- train.py
+++ train.py
@@ -16,8 +16,39 @@ from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from keras.layers import Conv2D, Dropout, Flatten, Dense, MaxPooling2D, GlobalAveragePooling2D
 from keras.models import Sequential
 from keras.callbacks import ModelCheckpoint
-import importlib
-importlib.import_module(data)
+from data import*
+
+
+def model():
+	# Adding trainable layer to the imported arch.
+	my_model = Sequential()
+
+	# add 2 fully connected layer to the end of the arch. and train it on our dataset
+	my_model.add(GlobalAveragePooling2D(input_shape=train_data.shape[1:]))
+	my_model.add(Dropout(0.4)) # to eliminate overfitting
+	my_model.add(Dense(1024, activation='relu'))
+	my_model.add(Dropout(0.4))
+	# last layer should be 3 perceptrons only because we have only 3 classes in our dataset
+	my_model.add(Dense(3, activation='softmax'))
+
+	print(my_model.summary())
+	return my_model
+
+
+
+def model_training():
+	# compile our model
+	my_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+	checkpoint_filepath = 'weights.hdf5'
+	my_checkpointer = ModelCheckpoint(filepath=checkpoint_filepath,
+	                               verbose=1, save_best_only=True)
+
+	# now our model is ready for training .. lets do that
+	my_model.fit(train_data, train_targets, 
+	          validation_data=(valid_data, valid_targets),
+	          epochs=60, batch_size=200, callbacks=[my_checkpointer], verbose=1)
+
 
 # Transfere Learning section
 # preprocess_input method is imported above from keras.applications.inception_resnet_v2
@@ -37,35 +68,3 @@ del train_imgs_preprocess, valid_imgs_preprocess, test_imgs_preprocess
 
 my_model = model()
 model_training()
-
-def model():
-	# Adding trainable layer to the imported arch.
-	my_model = Sequential()
-
-	# add 2 fully connected layer to the end of the arch. and train it on our dataset
-	my_model.add(GlobalAveragePooling2D(input_shape=train_data.shape[1:]))
-	my_model.add(Dropout(0.4)) # to eliminate overfitting
-	my_model.add(Dense(1024, activation='relu'))
-	my_model.add(Dropout(0.4))
-	# last layer should be 3 perceptrons only because we have only 3 classes in our dataset
-	my_model.add(Dense(3, activation='softmax'))
-
-	print(my_model.summary())
-	return my_model
-
-
-
-def model_training()
-	# compile our model
-	my_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-	checkpoint_filepath = 'weights.hdf5'
-	my_checkpointer = ModelCheckpoint(filepath=checkpoint_filepath,
-	                               verbose=1, save_best_only=True)
-
-	# now our model is ready for training .. lets do that
-	my_model.fit(train_data, train_targets, 
-	          validation_data=(valid_data, valid_targets),
-	          epochs=60, batch_size=200, callbacks=[my_checkpointer], verbose=1)
-
-
